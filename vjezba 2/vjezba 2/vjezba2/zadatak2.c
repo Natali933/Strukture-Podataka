62b6a63aebf74f0df33d35cb46122b13054f011b
Natali933 <natalipesic2005@gmail.com>
2025-10-30 12:09:40 +0100

    3.vjezba

--- vjezba 2/vjezba 2/vjezba2/zadatak2.c
+++ vjezba 2/vjezba 2/vjezba2/zadatak2.c
@@ -8,7 +8,7 @@ typedef struct person* Position;
 typedef struct person {
 	char name[50];
 	char surname[50];
-	int points;
+	int year;
 
 	Position Next;
 
@@ -17,37 +17,41 @@ typedef struct person {
 Position initialization();
 
 
-int UnosP(char[], char[], int, Position);
+int InsertB(char[], char[], int, Position);
 int Ispis(Position);
-int UnosK(char[], char[], int, Position);
-int Search(char[], Position); // radila sa normalnim Position ali sam htjela da mi izbaci sve ljude ako imaju ista prezimena
+int InsertE(char[], char[], int, Position);
+int SearchSurname(char[], Position); // radila sa normalnim Position ali sam htjela da mi izbaci sve ljude ako imaju ista prezimena
 int Delete(char[], char[], Position);
+Position Search(char[], char[], Position); //htjela sam po imenu i prezimenu ako netko slucajno ima isto prezime
+
+//funkcije za vjezbu 3
+Position SearchBef(char[], char[], Position); //isto po imenu i prezimenu
+int InsertBef(char[], char[], int, char[], char[], Position);
+int InsertAft(char[], char[], int, char[], char[], Position);
+int InFile(Position);
+int FromFile(Position);
+int ClearAll(Position);
+int Sort(Position);
+
+int InsertPerson(char[], char[], int*);
 
 
 int main() {
 	Person Head = { "", "", 0, NULL };
 	
 	char name[50], surname[50];
-	int points;
+	int year;
 	char choice[10];
+	char thisname[50], thissurname[50];
 
 	printf("UNOS NA POCETAK");
 	while (1) {
-		printf("\nUnesi ime, prezime, bodove(ili 'kraj' za prekid)");
+		int status = InsertPerson(name, surname, &year);
 		
-		if (scanf("%49s", name) != 1)
-			break;
+		if (status == 0) break;
+		if (status == -1) continue;
 		
-		if (strcmp(name, "kraj") == 0) 
-			break;
-
-		if (scanf("%49s %d", surname, &points) != 2) {
-			printf("\neispravan unos");
-			continue;
-		}
-		
-
-		UnosP(name, surname, points, &Head);
+		InsertB(name, surname, year, &Head);
 
 	}
 	
@@ -55,20 +59,12 @@ int main() {
 
 	printf("\nUNOS NA KRAJ");
 	while (1) {
-		printf("\nUnesi ime, prezime, bodove(ili 'kraj' za prekid)");
+		int status = InsertPerson(name, surname, &year);
 
-		if (scanf("%49s", name) != 1)
-			break;
+		if (status == 0) break;
+		if (status == -1) continue;
 
-		if (strcmp(name, "kraj") == 0)
-			break;
-
-		if (scanf("%49s %d", surname, &points) != 2) {
-			printf("\nneispravan unos");
-			continue;
-		}
-
-		UnosK(name, surname, points, &Head);
+		InsertE(name, surname, year, &Head);
 	}
 
 	Ispis(Head.Next);
@@ -79,7 +75,7 @@ int main() {
 		printf("neispravan unos");
 
 	if (strcmp(surname, "kraj") != 0) 
-		 Search(surname, Head.Next);
+		 SearchSurname(surname, Head.Next);
 
 	
 	printf("\nZelis li obrisati nekoga? (da/ne): ");
@@ -95,8 +91,60 @@ int main() {
 		Delete(name, surname, &Head);
 		
 	}
+	
+	Ispis(Head.Next);
+
+	printf("\nnapisi koga zelis dodati ispred koga(ili 'kraj' za preskok): ");
+
+	if (scanf("%49s", thisname) != 1)
+		printf("neispravan unos");
+
+	if (strcmp(thisname, "kraj") != 0) {
+		if (scanf("%49s %d", thissurname, &year) != 2)
+			printf("\neispravan unos");
+
+			printf("\nispred koga:");
+
+		if (scanf("%49s %49s", name, surname) != 2)
+			printf("\neispravan unos");
+
+		InsertBef(thisname, thissurname, year, name, surname, &Head);
+
+	}
+	Ispis(Head.Next);
+
+	printf("\nnapisi koga zelis dodati iza koga(ili 'kraj' za preskok): ");
+
+	if (scanf("%49s", thisname) != 1)
+		printf("neispravan unos");
+
+	if (strcmp(thisname, "kraj") != 0) {
+		if (scanf("%49s %d", thissurname, &year) != 2)
+			printf("\neispravan unos");
+
+		printf("\niza koga:");
+
+		if (scanf("%49s %49s", name, surname) != 2)
+			printf("\neispravan unos");
+
+		InsertAft(thisname, thissurname, year, name, surname, &Head);
+
+	}
+	Ispis(Head.Next);
+
+	InFile(Head.Next);
+
+	ClearAll(&Head); //jer je pri pozivanju FromFile samo dodao opet na vec postojece
+	
+	printf("\n--iscitano iz file-a--");
+	FromFile(&Head);
+	Ispis(Head.Next);
+
+	Sort(&Head);
+	printf("\n--sortirana lista--");
 	Ispis(Head.Next);
 	
+	return 0; 
 
 }
 
@@ -113,14 +161,14 @@ Position initialization()
 	return q;
 }
 
-int UnosP(char thisname[], char thissurname[], int x, Position p) 
+int InsertB(char thisname[], char thissurname[], int x, Position p) 
 {
 	Position q;
 	q = initialization();
 	
 	strcpy(q->name, thisname);
 	strcpy(q->surname, thissurname);
-	q->points = x;
+	q->year = x;
 	q->Next = p->Next;
 	p->Next = q;
 
@@ -133,14 +181,14 @@ int Ispis(Position p)
 
 	while (p != NULL)
 	{
-		printf("%s %s - %d\n", p->name, p->surname, p->points);
+		printf("%s %s - %d\n", p->name, p->surname, p->year);
 		p = p->Next;
 	}
 
 	return 0;
 }
 
-int UnosK(char thisname[], char thissurname[], int x, Position p)
+int InsertE(char thisname[], char thissurname[], int x, Position p)
 {
 	Position q;
 
@@ -151,7 +199,7 @@ int UnosK(char thisname[], char thissurname[], int x, Position p)
 
 	strcpy(q->name, thisname);
 	strcpy(q->surname, thissurname);
-	q->points = x;
+	q->year = x;
 
 	q->Next = p->Next;
 	p->Next = q;
@@ -160,11 +208,11 @@ int UnosK(char thisname[], char thissurname[], int x, Position p)
 
 }
 
-int  Search(char need[], Position p) {
+int  SearchSurname(char need[], Position p) { 
 	int found = 0;
 	while (p != NULL) {
 		if (strcmp(p->surname, need) == 0) {
-			printf("%s %s - %d\n", p->name, p->surname, p->points);
+			printf("%s %s - %d\n", p->name, p->surname, p->year);
 			found++;
 		}
 		p = p->Next;
@@ -183,6 +231,8 @@ int Delete(char thisname[], char thissurname[], Position p) {
 	while (curr != NULL){
 		if (strcmp(curr->name, thisname) == 0 && strcmp(curr->surname, thissurname) == 0) {
 			prev->Next = curr->Next;
+
+			curr->Next = NULL;
 			free(curr);
 			printf("\nosoba je pronadena");
 			return 0;
@@ -196,6 +246,176 @@ int Delete(char thisname[], char thissurname[], Position p) {
 		return -1;
 	
 }
+
+Position Search(char name[], char surname[], Position p) {
+	while (p != NULL && (strcmp(p->name, name) != 0 || strcmp(p->surname, surname) != 0))
+		p = p->Next;
+
+	if (p == NULL) return NULL;
+
+	return p;
+	
+}
+
+
+Position SearchBef(char name[], char surname[], Position p) {
+	while (p->Next != NULL && (strcmp(p->Next->name, name) != 0 || strcmp(p->Next->surname, surname) != 0)) //&& ima veci prioritet od || pa mi trebauju zagrade
+		p = p->Next;
+
+	if (p->Next == NULL) return NULL;
+
+	return p;
+}
+
+int InsertBef(char thisname[], char thissurname[], int x, char name[], char surname[], Position p) {
+	Position q, prev;
+
+	prev = SearchBef(name, surname, p);
+
+	if (prev == NULL) {
+		printf("\nnije pronadena osoba");
+		return -1;
+	}
+
+	q = initialization();
+
+	strcpy(q->name, thisname);
+	strcpy(q->surname, thissurname);
+	q->year = x;
+
+	q->Next = prev->Next;
+	prev->Next = q;
+		
+	return 0;
+
+}
+
+int InsertAft(char thisname[], char thissurname[], int x, char name[], char surname[], Position p) {
+	Position q, temp;
+
+	temp = Search(name, surname, p);
+
+	if (temp == NULL) {
+		printf("\nnije pronadena osoba");
+		return -1;
+	}
+
+	q = initialization();
+
+	if (q == 0) {
+		printf("\nalokacija pogresna");
+		return -1;
+	}
+
+	strcpy(q->name, thisname);
+	strcpy(q->surname, thissurname);
+	q->year = x;
+
+	q->Next = temp->Next;
+	temp->Next = q;
+
+	return 0;
+
+}
+
+int InFile(Position p) {
+	FILE* fp;
+	fp = fopen("zad.txt", "w");
+
+	if (fp == NULL) {
+		printf("greska pri otvaranju file-a");
+		exit(EXIT_FAILURE);
+	}
+
+	while (p != NULL)
+	{
+		fprintf(fp, "%s %s %d\n", p->name, p->surname, p->year);
+		p = p->Next;
+	}
+
+	fclose(fp);
+	return 0;
+}
+
+int FromFile(Position p) {
+	char name[50], surname[50];
+	int year;
+
+	FILE* fp;
+	fp = fopen("zad.txt", "r");
+
+	if (fp == NULL) {
+		printf("greska pri otvaranju file-a");
+		exit(EXIT_FAILURE);
+	}
+
+	while (fscanf(fp, "%s %s %d", name, surname, &year) == 3)
+	{
+		InsertE(name, surname, year, p);
+	}
+
+	fclose(fp);
+	return 0;
+}
+
+int ClearAll(Position p) {
+	Position temp;
+
+	while (p->Next != NULL) {
+		temp = p->Next;
+		p->Next = temp->Next;
+		free(temp);
+	}
+	return 0;
+}
+
+int Sort(Position p) {
+	Position j, prev_j,temp, end;
+
+	end = NULL;
+	while (p->Next != end) {
+		prev_j = p;
+		j = p->Next;
+		
+		while (j->Next != end) {
+			if (strcmp(j->surname, j->Next->surname) > 0 || (strcmp(j->surname, j->Next->surname) == 0 && strcmp(j->name, j->Next->name) > 0)) {
+				temp = j->Next;
+				prev_j->Next = temp;
+				j->Next = temp->Next;
+				temp->Next = j;
+
+				j = temp;
+			}
+
+			prev_j = j;
+			j = j->Next;
+		}
+		end = j;
+	}
+	return 0;
+
+}
+
+int InsertPerson(char name[], char surname[], int* year) {
+	printf("\nUnesi ime, prezime i godinu (ili 'kraj' za prekid): ");
+
+	if (scanf("%49s", name) != 1)
+		return 0; // greška u unosu
+
+	if (strcmp(name, "kraj") == 0)
+		return 0; // korisnik želi prekinuti
+
+	if (scanf("%49s %d", surname, year) != 2) {
+		printf("Neispravan unos! Pokušaj ponovo.\n");
+		while (getchar() != '\n'); // oèisti ulazni buffer
+		return -1;
+	}
+
+	return 1; // sve ok
+}
+
+
+
 
 
 
